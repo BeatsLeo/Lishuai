3df1691ecc7b48410c558a570cb86bdca84550e0
BeatsLeo <1059899674@qq.com>
2021-01-09 20:38:21 +0800

    w1

---
+++ 其他/未命名1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+	int main(void)
+{
+	printf("I love three things in the world.\nThe sun, the moon,and you.\n");
+	printf("The sun is for day,the moon is for night.\nAnd you are forever.	");
+	return 0;
+ } +
